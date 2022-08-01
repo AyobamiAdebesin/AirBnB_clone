86e8f5efbb8b3e1323d8264a7ad0ca67b10aa18c
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 00:38:46 +0100

    refactor: completed test for base model

--- models/test_base_model.py
+++ models/test_base_model.py
@@ -2,3 +2,19 @@
 from base_model import BaseModel
 
 my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print("My model before saving")
+print("="*20)
+print(my_model)
+
+my_model.save()
+print("\nMy model after saving")
+print("="*20)
+print(my_model)
+
+
+my_model_json = my_model.to_dict()
+print("\nJSON of my model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) -{}".format(key, type(my_model_json[key]), my_model_json[key]))
