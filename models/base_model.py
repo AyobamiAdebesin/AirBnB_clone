fde76131c2fcb48befa2c1385acbf5bd6405fd98
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 00:37:57 +0100

    refactor: finished implementation of base model

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
         Updates the public instance attribute updated_at with the
         current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = str(datetime.now())
 
     def to_dict(self):
         """
@@ -38,4 +38,5 @@ class BaseModel:
         """
         new_dict = self.__dict__
         new_dict['__class__'] = 'BaseModel'
-        new_dict['created_at'] = self.created.isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        return (new_dict)
