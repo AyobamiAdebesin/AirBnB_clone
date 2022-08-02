8c2b71e6fda59c30e663b509e9f1fe59fc85cd59
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 02:10:38 +0100

    updated BaseModel to initialize a class from a dictionary/JSON representation

--- models/base_model.py
+++ models/base_model.py
@@ -11,23 +11,34 @@ class BaseModel:
     other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialiazes the Base model atributes"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """A string representation of the Base Model"""
-        return '[BaseModel] ({}) {}'.format(self.id, self.__dict__)
+        return '[{}] ({}) <{}>'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Updates the public instance attribute updated_at with the
         current datetime
         """
-        self.updated_at = str(datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -35,6 +46,7 @@ class BaseModel:
         of the instance
         """
         new_dict = self.__dict__
-        new_dict['__class__'] = 'BaseModel'
+        new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return (new_dict)
