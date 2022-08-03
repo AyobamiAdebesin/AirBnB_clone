87461be22710993f4e79fb8b4ea3643f5fd0c70b
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-03 23:05:44 +0100

    refactor: finished implementing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """A File storage class"""
 import json
+
 
 class FileStorage:
     """
@@ -13,21 +14,40 @@ class FileStorage:
     def all(self):
         """Returns the dictionary __objects"""
         return (self.__objects)
-    
+
     def new(self, obj):
-        pass
+        """
+        Add a new element; obj, to the self.__objects dictionary
+        obj : obj is a class instance
+        """
+        obj_id = obj.__class__.__name__ + "." + obj.id
+        self.__objects[obj_id] = obj
 
     def save(self):
         """
         Serializes __objects to the JSON file path: __file_path
         """
+        # Create a dict to store as value, the dictionary representation
+        # of the class instance (obj).
+        json_object = {}
+        for key, obj in self.__objects.items():
+            json_object[key] = obj.to_dict()
+
         with open(self.__file_path, 'w', encoding="utf-8") as f:
-            json.dump(self.__objects, f)
+            json.dump(json_object, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
+        from models.base_model import BaseModel
         try:
             with open(self.__file_path, 'r', encoding="utf-8") as f:
-                self.__objects = json.load(f)
+                json_object = json.load(f)
+            # Loop through the json_object keys to extract the "__class__"
+            # attribute and use the value to create instances
+            for key, value, in json_object.items():
+                self.new(BaseModel(**value))
+                #class_name = json_object[key]['__class__']
+                #object_dict = json_object[key]
+                #self.__objects[key] = self.new(BaseModel(**object_dict))
         except FileNotFoundError:
             pass
