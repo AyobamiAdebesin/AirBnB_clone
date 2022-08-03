5bcf248d7bd97444a548e854eb2f0f04a27eb9d7
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-03 22:59:35 +0100

    refactor: fixed import errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-"""Instantiates FileStorage instance"""
-from engine.file_storage import FileStorage
+#!/usr/bin/python3
+"""Instantiates FileStorage class"""
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
