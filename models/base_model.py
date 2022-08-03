73700286632897ceb2616c07260c257421eccafb
Akeh9ja <wilfreddbest1@gmail.com>
2022-08-03 19:57:01 +0100

    test models file

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 import cmd
 from datetime import datetime
-import storage
+# import storage
 
 class BaseModel:
     """
@@ -41,7 +41,7 @@ class BaseModel:
         current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        #storage.save()
 
     def to_dict(self):
         """
