d5ee0af22f2a9bcf16e208fb6b69a4f61ba55fb9
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-05 13:00:28 +0100

    Refactor: modified test script

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 from models import storage
-
+from models.base_model import BaseModel
 
 all_objs = storage.all()
-obj_len = len(all_objs)
-if len(all_objs) == 0:
-    print("{} object(s) have been created".format(len(all_objs)))
-else:
-    print("{} objects reloaded".format(obj_len))
-    print("--------Reloaded objects-------------")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
 
-print("--------Creating a new object---------")
+print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
